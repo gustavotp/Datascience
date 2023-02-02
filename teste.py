2c9cdc632b10466dc61cedf3767c9b99bdba4610
gustavotp <gustavopalmeira@gmail.com>
2023-02-01 21:54:07 -0300

    Teste da aula1

---
+++ teste.py
@@ -0,0 +1,19 @@
+X=2.34
+Y=3.11
+S= (X+Y)
+print(S)
+print(type(S))
+X=2.35
+Y=3.15
+S= (X+Y)
+print(S)
+print(type(S))
+X='2'
+Y='3'
+S= (X+Y)
+print(S)
+print(type(S))
+v1=float(input('digite nota 1'))
+av2=float(input('digite nota 2'))
+media = (av1+av2)/2
+print(media)
