00e544ff76808fad079fa1fdf7b47c99e2d8c60d
Gustavo Palmeira <gustavopalmeira@gmail.com>
2023-03-15 21:20:12 -0300

    Add files via upload

---
+++ 2023-03-15_aula05.r
@@ -0,0 +1,15 @@
+# arqu1<- read.table(file= "teste.csv",sep = ",", header=TRUE)
+# print(head(arqu1))
+
+# arq2<- read.table(file="C:/Users/aluno/Documents/teste.csv",sep =",", header=TRUE)
+# print(head(arq2))
+# x<- "texto em R"
+# write.table (x, "file1.csv")
+# 
+# library(readxl)
+# dados2<- read_xlsx("C:/Users/aluno/Documents/dados1.xlsx")
+# print(head(dados2))
+
+# library(curl)
+# arq<- read.csv(curl("https://raw.githubusercontent.com/dourivaljunior/Curso-de-Python/main/arquivo_departamento.csv"))
+# print(head(arq))
