9f5a81589303775514c953275aaffe55a3a63b4c
Gustavo Palmeira <gustavopalmeira@gmail.com>
2023-03-15 21:48:47 -0300

    Add files via upload

---
+++ 2023-03-15_aula05-estatistica.r
@@ -0,0 +1,37 @@
+#vetor1<- c("R","Python","Java")
+#print(vetor1)
+#print(vetor1[1])
+#print(vetor1[1:3])
+#print(vetor1[c(1,3)])
+
+#vetor2<- c(2,5,20,15,10)
+#print(vetor2)
+#print(vetor2[2])
+#print(sort(vetor2))
+
+# vetor3<- c(TRUE,FALSE,1,2)
+# print(vetor3)
+
+# vetor4<- c("TRUE","FALSE",1,2)
+# print(vetor4)
+
+#lista<- list("Maria", 22L)
+#print(lista[1])
+#print(lista[2])
+#novalista<- append(lista,"RG123321")
+#print(novalista)
+
+# mat1<-matrix(c(1,5,10,30,15,8),nrow=3,ncol=2,byrow=TRUE)
+# print(mat1)
+
+# vec1<-c(3,4,5)
+# vec2<-c(9,10,11)
+# vec3<-c(13,14,15)
+# mat3<-rbind(vec1,vec2,vec3)
+# print(mat3)
+
+# vec1<-c(3,4,5)
+# vec2<-c(9,10,11)
+# vec3<-c(13,14,15)
+# mat4<- cbind(vec1,vec2,vec3)
+# print(mat4)
